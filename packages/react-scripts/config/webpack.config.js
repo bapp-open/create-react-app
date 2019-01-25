396e062984cb496399aef6eea123426ef6348b24
Stefan Zaporojan <stefan@zaporojan.com>
2019-01-25 17:51:57 +0200

    added support for class-properties

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -395,6 +395,10 @@ module.exports = function(webpackEnv) {
                     require('@babel/plugin-proposal-decorators').default,
                     { decoratorsBeforeExport: true },
                   ],
+                  [
+                    require('@babel/plugin-proposal-class-properties').default,
+                    { "loose": true },
+                  ],
                 ],
                 // This is a feature of `babel-loader` for webpack (not Babel itself).
                 // It enables caching results in ./node_modules/.cache/babel-loader/
